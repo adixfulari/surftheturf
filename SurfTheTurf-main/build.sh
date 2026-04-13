90a06b7188b33f9cf823f65588fd48eb501d6955
= <prajyotlari@gmail.com>
2026-04-14 01:42:59 +0530

    Fix build configuration for Render deployment

--- SurfTheTurf-main/build.sh
+++ SurfTheTurf-main/build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+set -o errexit
+
+echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Running migrations..."
 python manage.py migrate
+
+echo "Build complete!"
